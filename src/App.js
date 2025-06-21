28bac2534cb382b58181c99a5b136a4ac86f0e9a
Vansh Baldha <baldhavansh2505@gmail.com>
2025-06-21 12:20:02 +0530

    project done

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React, { createContext, useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+
+import Home from "../src/Pages/Home";
+import About from "../src/Pages/About";
+import Shop from "../src/Pages/Shop";
+import Contact from "../src/Pages/Contact";
+import Blog from "../src/Pages/Blog";
+import Header from "../src/Components/Header/Navbar";
+import Footer from "../src/Components/Footer/Footer";
+import ProductDetails from "./Pages/ProductDetails";
+import NotFound from "./Pages/NotFound";
+import ScrollToTop from "./Components/ScrollButton/ScrollToTop";
+import Authentication from "./Pages/Authentication";
+import ResetPass from "./Components/Authentication/Reset/ResetPass";
+import BlogDetails from "./Components/Blog/BlogDetails/BlogDetails";
+import TermsConditions from "./Pages/TermsConditions";
+import ShoppingCart from "./Components/ShoppingCart/ShoppingCart";
+import Popup from "./Components/PopupBanner/Popup";
+import { Toaster } from "react-hot-toast";
+
+export const ThemeContext = createContext(null);
+
+const App = () => {
+  const [theme, setTheme] = useState(() =>
+    localStorage.getItem("toggle") === "true" ? "light" : "dark"
+  );
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
+
+  useEffect(() => {
+    localStorage.setItem("toggle", theme === "light" ? "true" : "false");
+  }, [theme]);
+
+  return (
+    <>
+      <ThemeContext.Provider value={{ theme, toggleTheme }}>
+        <div className="App" id={theme}>
+          <Popup />
+          <ScrollToTop />
+          <BrowserRouter>
+            <Header theme={theme} toggleTheme={toggleTheme}/>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/shop" element={<Shop />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/blog" element={<Blog />} />
+              <Route path="/product" element={<ProductDetails />} />
+              <Route path="/loginSignUp" element={<Authentication />} />
+              <Route path="/resetPassword" element={<ResetPass />} />
+              <Route path="/BlogDetails" element={<BlogDetails />} />
+              <Route path="/terms" element={<TermsConditions />} />
+              <Route path="/cart" element={<ShoppingCart />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+            <Footer />
+            <Toaster />
+          </BrowserRouter>
+        </div>
+      </ThemeContext.Provider>
+    </>
+  );
+};
+
+export default App;
